5d83e2f1081196d3e497bbe630aeeeefb5869a72
thiago2908 <167465727+thiago2908@users.noreply.github.com>
2024-04-23 16:32:22 -0300

    eu programei o nome, sobrenome e telefone no get e no set
    eu programei o nome, sobrenome e telefone no get e no set

---
+++ ListaDeContatos/Contato.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatos
+{
+    internal class Contato
+    {
+        // declaração de variaveis.
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        // Propiedades. são os métodos "get" e "set".
+        // get = leitura
+        // set = definição
+        // é possivel ser somente leitura, ou somente definição
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+
+        }
+        public string Telefone
+        {
+            get
+            {
+                return sobrenome;
+
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+
+                }
+                else
+                {
+                    telefone = "(00) 000-000";
+                }
+            }
+        }
+    }
+}
+
+
+
+
+    
